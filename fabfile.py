4495d34fe1a88089e68ebae82d71f395cc34c853
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-03-01 11:21:46 -0600

    first pass at deployment script

---
+++ fabfile.py
@@ -0,0 +1,133 @@
+import sys
+import os
+import deploy_config
+
+# Add current directory to path.
+sys.path.append(os.path.dirname(__file__))
+
+from fabric.api import *
+
+path = '/home/crisisnet/crisisnet'
+venv = '/home/crisisnet/crisisnet/venv'
+release_file = '/home/crisisnet/releases.crisisnet'
+
+@task
+def staging():
+    env.host_string = deploy_config.STAGING_HOST
+    env.user = deploy_config.STAGING_USER
+    env.password = deploy_config.STAGING_PASSWORD
+    env.key_filename = ''
+    env.branch = 'development'
+    env.upstart_script = 'crisisnet.conf'
+    env.nginx_conf = ''
+
+
+@task
+def production():
+    env.host_string = ''
+    env.user = ''
+    env.key_filename = ''
+    env.branch = 'master'
+    env.upstart_script = ''
+    env.nginx_conf = ''
+
+
+@task
+def add_apt_repo():
+    """
+    Adds nodejs PPA.
+    This is needed only the first time a new instance is configure.
+    """
+    sudo('apt-get install python-software-properties')
+    sudo('add-apt-repository ppa:chris-lea/node.js')
+    sudo('apt-get update')
+
+
+
+def install_deps():
+    """
+    Installs os and base packages.
+    """
+    deps = ['python-software-properties python g++ make nodejs git']
+    for dep in deps:
+        sudo('apt-get install -y --no-upgrade %s' % dep)
+
+
+def check_upstart(upstart_conf):
+    """
+    Checks if upstart exists; if not, upstart job is created.
+    If it exists and is different from the checked-in version, it's updated.
+    """
+    sudo('test -f /etc/init/crisisnet.conf || cp etc/%s /etc/init/crisisnet.conf' % upstart_conf)
+    sudo('diff etc/%s /etc/init/crisisnet.conf || cp etc/%s /etc/init/crisisnet.conf' % (upstart_conf, upstart_conf))
+
+
+def prepare_log_file():
+    """
+    Creates logfile and changes permissions so that user `cn` can write to it.
+    """
+    sudo('test -f /var/log/crisisnet.log || touch /var/log/crisisnet.log')
+    sudo('chown cn:cn /var/log/crisisnet.log')
+
+
+def nginx_sites_enabled(nginx_conf):
+    """
+    Checks if nginx sites-enabled configs exists.
+    Update/create if it doesn't exist or is different.
+    """
+    sudo('test -f /etc/nginx/sites-enabled/crisisnet || cp etc/%s /etc/nginx/sites-enabled/crisisnet' % nginx_conf)
+    sudo('diff etc/%s /etc/nginx/sites-enabled/crisisnet || cp etc/%s /etc/nginx/sites-enabled/crisisnet' % (nginx_conf, nginx_conf))
+
+
+@task
+@parallel
+def deploy(branch=None):
+    """
+    SSH to ec2, updates git code, restarts app server.
+    """
+    branch = branch or env.branch
+    install_deps()
+    prepare_log_file()
+
+    # Check for first deploy.
+    run("test -d %s || git clone https://github.com/ushahidi/crisisnet.git %s" % (path, path))
+    with cd(path):
+        #run('git branch --set-upstream %s origin/%s' % (branch, branch))
+        run('git fetch')
+        run('git checkout %s && git pull' % branch)
+        do_release()
+        record_release()
+
+
+def do_release():
+    sudo('npm install')
+    check_upstart(env.upstart_start)
+    sudo('service crisisnet status && restart crisisnet || start crisisnet')
+    #nginx_sites_enabled(env.nginx_conf)
+    #sudo('service nginx status || service nginx start')
+    #sudo('service nginx reload')
+
+
+def record_release():
+    """
+    Records the git commit version so that we can rollback.
+    """
+    current_release = run("git rev-parse HEAD")
+    last_release = run("tail -n 1 %s" % release_file, warn_only=True)
+    if last_release.failed:
+        run("echo %s > %s" % (current_release, release_file))
+    elif current_release != last_release:
+        run("echo %s >> %s" % (current_release, release_file))
+
+
+@task
+@parallel
+def rollback(num=1):
+    """
+    Rollsback git version to a previous release.
+    """
+    num = num + 1
+    with cd(path):
+        release_version = run("tail -n %s %s | head -n 1" % (num, release_file))
+        run('git checkout %s' % release_version)
+        do_release()

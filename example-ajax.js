01e7d7b74196e3e69aee7104ca2cec1e5fec7c84
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-16 12:49:30 -0500

    adding GA

--- example-ajax.js
+++ example-ajax.js
@@ -8,3 +8,25 @@ $.ajax({
     console.log(data);
   }
 });
+
+$.ajax({
+  url: 'http://localhost:8083/request',
+  dataType: "json",
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer 532d1bb6bbcdd1862d6e15b4');
+  },
+  success: function (data) {
+    console.log(data);
+  }
+});
+
+$.ajax({
+  url: 'http://localhost:8083/item?location=36.821946,-1.292066',
+  dataType: "json",
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer 532d1bb6bbcdd1862d6e15b4');
+  },
+  success: function (data) {
+    console.log(data);
+  }
+});
